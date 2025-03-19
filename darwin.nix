ae68aa196300add2223e2c17c4481328c337a823
valrus <ian@mccowan.space>
2025-03-19 11:31:28 -0700

    remove depr nix-daemon enable

--- darwin.nix
+++ darwin.nix
@@ -65,10 +65,6 @@
   programs.zsh.enable = true;
   programs.fish.enable = true;
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
-
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
