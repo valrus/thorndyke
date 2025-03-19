a8838fd9ab59e32ce2010856baf4c77363ba44de
valrus <ian@mccowan.space>
2025-03-19 11:30:40 -0700

    Add mac-app-util; unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,17 +2,25 @@
   description = "Darwin configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
 
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    mac-app-util.url = "github:hraban/mac-app-util";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nix-darwin, ... }:
-  {
+  outputs = inputs@{
+    self
+    , nixpkgs
+    , home-manager
+    , nix-darwin
+    , mac-app-util
+    , ...
+  }: {
     defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
 
     # Build darwin flake using:
@@ -27,11 +35,17 @@
 
         modules = [
           ./darwin.nix
+          mac-app-util.darwinModules.default
+
           inputs.home-manager.darwinModules.home-manager {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
-              users.valrus = import ./home;
+              users.valrus.imports = [
+                ./home
+                mac-app-util.homeManagerModules.default
+              ];
+              # import ./home;
             };
           }
         ];
