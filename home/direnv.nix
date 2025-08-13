e4f5ff1831c05fe6109ff8df0ff50949bcdfdb7a
valrus <ian@mccowan.space>
2025-08-13 08:34:44 -0700

    Add comment re: no explicit fish integration

--- home/direnv.nix
+++ home/direnv.nix
@@ -4,6 +4,8 @@
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
+    # This is default, adding it will only complain about multiple defs
+    # enableFishIntegration = true;
     nix-direnv.enable = true;
   };
 }
